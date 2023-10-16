e001e243acac9e17c5ff0899739063a9ed7a07c2
Gabriel Carneiro <carneiro.development@gmail.com>
2023-10-16 01:23:07 -0400

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import Table from "./components/BasicTable";
+
+const App = () => {
+  return (
+    <div>
+      <Table />
+    </div>
+  );
+};
+
+export default App;
